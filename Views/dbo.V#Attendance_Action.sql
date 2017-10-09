e958990e340f7f354c80905d8b7bfc5a8a4b05bb
hadaeghimohsen <hadaeghimohsen@yahoo.com>
2017-10-09 20:57:00 +0330

    Update 1396/07/17 20:56PM

--- Views/dbo.V#Attendance_Action.sql
+++ Views/dbo.V#Attendance_Action.sql
@@ -4,51 +4,48 @@ SET ANSI_NULLS ON
 GO
 
 
+
 CREATE VIEW [dbo].[V#Attendance_Action]
 AS
-SELECT  a.FIGH_FILE_NO ,
-        a.NAME_DNRM ,
-        a.ATTN_DATE ,
-        dbo.GET_MTOS_U(a.ATTN_DATE) AS ATTN_DATE_DNRM ,
-        CAST(a.ENTR_TIME AS VARCHAR(10)) AS ENTR_TIME ,
-        CAST(a.EXIT_TIME AS VARCHAR(10)) AS EXIT_TIME ,
-        ( SELECT    CAST(DERS_NUMB AS VARCHAR(10)) + ' ,'
-          FROM      dbo.Attendance at
-          WHERE     at.FIGH_FILE_NO = a.FIGH_FILE_NO
-                    AND at.ATTN_DATE = a.ATTN_DATE
-        FOR
-          XML PATH('')
-        ) AS DREN_NUMBS ,
-        ( SELECT    ISNULL(at.ATTN_DESC, '') + ' ,'
-          FROM      dbo.Attendance at
-          WHERE     at.FIGH_FILE_NO = a.FIGH_FILE_NO
-                    AND at.ATTN_DATE = a.ATTN_DATE
-        FOR
-          XML PATH('')
-        ) AS ATTN_DESCS ,
-        ( SELECT    SUM(ISNULL(pm.AMNT, 0))
-          FROM      dbo.Payment_Method pm
-          WHERE     pm.FIGH_FILE_NO_DNRM = a.FIGH_FILE_NO
-                    AND CAST(pm.ACTN_DATE AS DATE) = a.ATTN_DATE
-        ) AS PYMT_AMNT ,
-        ( SELECT    
-                    CASE pm.RCPT_MTOD
-                      WHEN '001' THEN N'نقدی'
-                      WHEN '003' THEN pm.FLOW_NO
-                    END + ','
-          FROM      dbo.Payment_Method pm
-          WHERE     pm.FIGH_FILE_NO_DNRM = a.FIGH_FILE_NO
-                    AND CAST(pm.ACTN_DATE AS DATE) = a.ATTN_DATE
-        FOR
-          XML PATH('')
-        ) AS PYMT_DESC
-FROM    dbo.Attendance a
-GROUP BY a.FIGH_FILE_NO ,
-        a.NAME_DNRM ,
-        a.ATTN_DATE ,
-        dbo.GET_MTOS_U(a.ATTN_DATE) ,
-        CAST(a.ENTR_TIME AS VARCHAR(10)) ,
-        CAST(a.EXIT_TIME AS VARCHAR(10));
+    SELECT  a.FIGH_FILE_NO ,
+            a.NAME_DNRM ,
+            a.ATTN_DATE ,
+            dbo.GET_MTOS_U(a.ATTN_DATE) AS ATTN_DATE_DNRM ,
+            CAST(MIN(a.ENTR_TIME) AS VARCHAR(10)) AS ENTR_TIME ,
+            CAST(MAX(a.EXIT_TIME) AS VARCHAR(10)) AS EXIT_TIME,
+            ( SELECT    CAST(DERS_NUMB AS VARCHAR(10)) + ' ,'
+              FROM      dbo.Attendance at
+              WHERE     at.FIGH_FILE_NO = a.FIGH_FILE_NO
+                        AND at.ATTN_DATE = a.ATTN_DATE
+            FOR
+              XML PATH('')
+            ) AS DREN_NUMBS ,
+            ( SELECT    ISNULL(at.ATTN_DESC, '') + ' ,'
+              FROM      dbo.Attendance at
+              WHERE     at.FIGH_FILE_NO = a.FIGH_FILE_NO
+                        AND at.ATTN_DATE = a.ATTN_DATE
+            FOR
+              XML PATH('')
+            ) AS ATTN_DESCS ,
+            ( SELECT    SUM(ISNULL(pm.AMNT, 0))
+              FROM      dbo.Payment_Method pm
+              WHERE     pm.FIGH_FILE_NO_DNRM = a.FIGH_FILE_NO
+                        AND CAST(pm.ACTN_DATE AS DATE) = a.ATTN_DATE
+            ) AS PYMT_AMNT ,
+            ( SELECT    CASE pm.RCPT_MTOD
+                          WHEN '001' THEN N'نقدی'
+                          WHEN '003' THEN pm.FLOW_NO
+                        END
+              FROM      dbo.Payment_Method pm
+              WHERE     pm.FIGH_FILE_NO_DNRM = a.FIGH_FILE_NO
+                        AND CAST(pm.ACTN_DATE AS DATE) = a.ATTN_DATE
+            ) AS PYMT_DESC
+    FROM    dbo.Attendance a
+    GROUP BY a.FIGH_FILE_NO ,
+            a.NAME_DNRM ,
+            a.ATTN_DATE ,
+            dbo.GET_MTOS_U(a.ATTN_DATE) ;
+
 
 
 
