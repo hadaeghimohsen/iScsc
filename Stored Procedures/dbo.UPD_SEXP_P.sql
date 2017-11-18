addd8ea2e6bc4551883bdf26e6bfb2e6e9fb5218
Mohsen Hadaeghi <hadaeghimohsen@yahoo.com>
2017-11-18 16:16:37 +0330

    Update 1396/08/27 16:16PM

--- Stored Procedures/dbo.UPD_SEXP_P.sql
+++ Stored Procedures/dbo.UPD_SEXP_P.sql
@@ -23,6 +23,9 @@ BEGIN
              ,@Qnty SMALLINT
              ,@FighFileNo BIGINT
              ,@CbmtCodeDnrm BIGINT
+             ,@MtodCodeDnrm BIGINT
+             ,@CtgyCodeDnrm BIGINT
+             ,@ExpnCode BIGINT
              ,@TranStat VARCHAR(3)
              ,@TranBy VARCHAR(250)
              ,@TranDate DATE
@@ -34,11 +37,14 @@ BEGIN
       SELECT @Rqid = @X.query('Request').value('(Request/@rqid)[1]', 'BIGINT')
             ,@PymtCashCode = @X.query('Request/Payment').value('(Payment/@cashcode)[1]', 'BIGINT')
             ,@PymtPydtCode = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@code)[1]', 'BIGINT')
+            ,@ExpnCode = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@expncode)[1]', 'BIGINT')
             ,@ExpnPric = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@expnpric)[1]', 'BIGINT')
             ,@PydtDesc = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@pydtdesc)[1]', 'NVARCHAR(250)')
             ,@Qnty = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@qnty)[1]', 'SMALLINT')
             --,@FighFileNo = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@fighfileno)[1]', 'BIGINT')
             ,@CbmtCodeDnrm = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@cbmtcodednrm)[1]', 'BIGINT')
+            ,@MtodCodeDnrm = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@mtodcodednrm)[1]', 'BIGINT')
+            ,@CtgyCodeDnrm = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@ctgycodednrm)[1]', 'BIGINT')
             ,@TranStat = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@transtat)[1]', 'VARCHAR(3)')
             ,@TranBy = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@tranby)[1]', 'VARCHAR(250)')
             ,@TranDate = @X.query('Request/Payment/Payment_Detail').value('(Payment_Detail/@trandate)[1]', 'DATE')
@@ -57,7 +63,7 @@ BEGIN
            FROM dbo.Club_Method
           WHERE code = @CbmtCodeDnrm;
       
-      IF @TranStat IS NULL
+      IF @TranStat IS NULL OR @TranStat = '003'
       BEGIN
          SELECT @TranBy = NULL
                ,@TranDate = NULL
@@ -66,16 +72,52 @@ BEGIN
                ,@TranCtgyCode = NULL
                ,@TranExpnCode = NULL;                      
       END
+      ELSE IF @TranStat IN ( '001', '002' )
+      BEGIN
+         SELECT @TranMtodCode = MTOD_CODE
+           FROM dbo.Category_Belt
+          WHERE CODE = @TranCtgyCode;
+      END
+      
+      IF @TranStat = '002'
+      BEGIN
+         DECLARE @TmpFighFileNo BIGINT
+                ,@TmpCbmtCode BIGINT
+                ,@TmpMtodCode BIGINT
+                ,@TmpCtgyCode BIGINT
+                ,@TmpExpnCode BIGINT;
+         
+         -- ثبت ایتم های فعلی
+         SELECT @TmpFighFileNo = @FighFileNo
+               ,@TmpCbmtCode   = @CbmtCodeDnrm
+               ,@TmpMtodCode   = @MtodCodeDnrm
+               ,@TmpCtgyCode   = @CtgyCodeDnrm
+               ,@TmpExpnCode   = @ExpnCode;         
+         
+         SELECT @FighFileNo = (SELECT COCH_FILE_NO FROM dbo.Club_Method WHERE CODE = @TranCbmtCode)
+               ,@CbmtCodeDnrm = @TranCbmtCode
+               ,@MtodCodeDnrm = @TranMtodCode
+               ,@CtgyCodeDnrm = @TranCtgyCode
+               ,@ExpnCode = @TranExpnCode;
+         
+         SELECT @TranCbmtCode   = @TmpCbmtCode
+               ,@TranMtodCode   = @TmpMtodCode
+               ,@TranCtgyCode   = @TmpCtgyCode
+               ,@TranExpnCode   = @TmpExpnCode;
+      END
       
       UPDATE Payment_Detail
          SET EXPN_PRIC = @ExpnPric
             ,PYDT_DESC = @PydtDesc
             ,QNTY = @Qnty  
-            ,FIGH_FILE_NO = @FighFileNo
+            ,FIGH_FILE_NO   = @FighFileNo
             ,CBMT_CODE_DNRM = @CbmtCodeDnrm
-            ,TRAN_STAT = @TranStat
-            ,TRAN_BY = @TranBy
-            ,TRAN_DATE = @TranDate
+            ,MTOD_CODE_DNRM = @MtodCodeDnrm
+            ,CTGY_CODE_DNRM = @CtgyCodeDnrm
+            ,EXPN_CODE      = @ExpnCode
+            ,TRAN_STAT      = @TranStat
+            ,TRAN_BY        = @TranBy
+            ,TRAN_DATE      = @TranDate
             ,TRAN_CBMT_CODE = @TranCbmtCode
             ,TRAN_MTOD_CODE = @TranMtodCode
             ,TRAN_CTGY_CODE = @TranCtgyCode
