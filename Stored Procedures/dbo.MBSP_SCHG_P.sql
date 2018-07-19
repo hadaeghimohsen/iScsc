b518b384ae0fe5bf26f879d7f3a216b8f1f99c01
Mohsen Hadaeghi <hadaeghimohsen@yahoo.com>
2018-07-19 13:59:36 +0430

    Update

--- Stored Procedures/dbo.MBSP_SCHG_P.sql
+++ Stored Procedures/dbo.MBSP_SCHG_P.sql
@@ -24,12 +24,16 @@ BEGIN
 	           @OrgnRqid BIGINT,
 	           @RqroRwno SMALLINT,	           
    	        @FileNo BIGINT,
-   	        @CbmtCode BIGINT;
+   	        @CbmtCode BIGINT,
+   	        @CtgyCode BIGINT,
+   	        @EditPymt VARCHAR(3);
    	
 	   SELECT @Rqid     = @X.query('//Request').value('(Request/@rqid)[1]'    , 'BIGINT')
 	         ,@RqroRwno = @X.query('//Request_Row').value('(Request_Row/@rwno)[1]', 'SMALLINT')
 	         ,@FileNo   = @X.query('//Request_Row').value('(Request_Row/@fileno)[1]', 'BIGINT')
-	         ,@CbmtCode = @X.query('//Member_Ship').value('(Member_Ship/@cbmtcode)[1]', 'BIGINT');
+	         ,@CbmtCode = @X.query('//Member_Ship').value('(Member_Ship/@cbmtcode)[1]', 'BIGINT')
+	         ,@CtgyCode = @X.query('//Member_Ship').value('(Member_Ship/@ctgycode)[1]', 'BIGINT')
+	         ,@EditPymt = @X.query('//Member_Ship').value('(Member_Ship/@editpymt)[1]', 'BIGINT');
 	   
 	   SET @OrgnRqid = @Rqid;
 	   
@@ -84,8 +88,10 @@ BEGIN
          AND RECT_CODE = '004';
       
       -- 1396/11/08 * بررسی اینکه آیا برنامه کلاسی عوض شده است یا خیر
-      DECLARE @OldCbmtCode BIGINT;
+      DECLARE @OldCbmtCode BIGINT,
+              @OldCtgyCode BIGINT;
       SELECT @OldCbmtCode = fp.CBMT_CODE
+            ,@OldCtgyCode = fp.CTGY_CODE
         FROM dbo.Member_Ship m, dbo.Fighter_Public fp
        WHERE m.RQRO_RQST_RQID = @Rqid
          AND m.RECT_CODE = '004'
@@ -93,7 +99,7 @@ BEGIN
          AND m.FGPB_RECT_CODE_DNRM = fp.RECT_CODE
          AND m.FIGH_FILE_NO = fp.FIGH_FILE_NO;
       
-      IF @CbmtCode != @OldCbmtCode
+      IF (@CbmtCode != @OldCbmtCode) OR (@CtgyCode != @OldCtgyCode)
       BEGIN
          DECLARE @PrvnCode VARCHAR(3)
                 ,@RegnCode VARCHAR(3);
@@ -111,18 +117,25 @@ BEGIN
          --SET @CbmtCode = @CbmtCode;
          EXEC PBL_RQST_F @X;
          
-         SELECT @Rqid = R.RQID
+         SELECT @Rqid = MAX(R.RQID)
            FROM Request R
           WHERE R.RQST_RQID = @Rqid
             AND R.RQST_STAT = '001'
             AND R.RQTP_CODE = '002'
-            AND R.RQTT_CODE = '004';
+            AND R.RQTT_CODE = '004'
+            AND CAST(R.RQST_DATE AS DATE) = CAST(GETDATE() AS DATE)
+            AND r.CRET_BY = UPPER(SUSER_NAME());
          
          UPDATE dbo.Fighter_Public
             SET CBMT_CODE = @CbmtCode
                ,COCH_FILE_NO = (SELECT COCH_FILE_NO FROM dbo.Club_Method WHERE CODE = @CbmtCode)
+               ,CTGY_CODE = @CtgyCode
           WHERE RQRO_RQST_RQID = @Rqid
             AND FIGH_FILE_NO = @FileNo;
+         
+         UPDATE dbo.Fighter
+            SET CTGY_CODE_DNRM = @CtgyCode
+          WHERE FILE_NO = @FileNo;
          
          SET @X = '<Process><Request rqid="" rqtpcode="002" rqttcode="004" regncode="" prvncode=""><Request_Row fileno=""></Request_Row></Request></Process>';
          SET @X.modify('replace value of (/Process/Request/@rqid)[1] with sql:variable("@Rqid")');
@@ -130,7 +143,7 @@ BEGIN
          SET @X.modify('replace value of (/Process/Request/@prvncode)[1] with sql:variable("@PrvnCode")');
          SET @X.modify('replace value of (/Process/Request/Request_Row/@fileno)[1] with sql:variable("@FileNo")');
          EXEC PBL_SAVE_F @X;
-         
+
          DECLARE @FgpbRwno INT
                 ,@CochFileNo BIGINT;
          
@@ -149,6 +162,7 @@ BEGIN
          UPDATE dbo.Payment_Detail
             SET CBMT_CODE_DNRM = @CbmtCode
                ,FIGH_FILE_NO = @CochFileNo
+               --,CTGY_CODE_DNRM = @CtgyCode
           WHERE PYMT_RQST_RQID = @OrgnRqid;          
       END
          
