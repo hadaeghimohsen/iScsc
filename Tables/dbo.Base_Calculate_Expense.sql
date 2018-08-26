70d476c522be288a2356bfdc21804a12e405f72a
Mohsen Hadaeghi <hadaeghimohsen@yahoo.com>
2018-08-26 10:11:30 +0430

    Update

--- Tables/dbo.Base_Calculate_Expense.sql
+++ Tables/dbo.Base_Calculate_Expense.sql
@@ -15,7 +15,8 @@ CREATE TABLE [dbo].[Base_Calculate_Expense]
 [CRET_BY] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CRET_DATE] [datetime] NULL,
 [MDFY_BY] [varchar] (250) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[MDFY_DATE] [datetime] NULL
+[MDFY_DATE] [datetime] NULL,
+[MIN_NUMB_ATTN] [smallint] NULL
 ) ON [PRIMARY]
 GO
 SET QUOTED_IDENTIFIER ON
@@ -173,6 +174,8 @@ GO
 EXEC sp_addextendedproperty N'MS_Description', N'زیر گروه', 'SCHEMA', N'dbo', 'TABLE', N'Base_Calculate_Expense', 'COLUMN', N'CTGY_CODE'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'کد آیتم درآمد / هزینه', 'SCHEMA', N'dbo', 'TABLE', N'Base_Calculate_Expense', 'COLUMN', N'EPIT_CODE'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'حداقل تعداد حضوری مشترک', 'SCHEMA', N'dbo', 'TABLE', N'Base_Calculate_Expense', 'COLUMN', N'MIN_NUMB_ATTN'
 GO
 EXEC sp_addextendedproperty N'MS_Description', N'گروه', 'SCHEMA', N'dbo', 'TABLE', N'Base_Calculate_Expense', 'COLUMN', N'MTOD_CODE'
 GO
