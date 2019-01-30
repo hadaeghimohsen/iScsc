645cc10d3018e1aaa19e915fce44c093beb99c12
Mohsen Hadaeghi <hadaeghimohsen@yahoo.com>
2019-01-30 15:25:16 +0330

    Update

--- Functions/dbo.VF$Request_Changing.sql
+++ Functions/dbo.VF$Request_Changing.sql
@@ -2,79 +2,105 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE FUNCTION [dbo].[VF$Request_Changing](
-   @FileNo BIGINT
-)
+CREATE FUNCTION [dbo].[VF$Request_Changing] ( @FileNo BIGINT )
 RETURNS TABLE
-AS RETURN
-(
-   SELECT
-           T.RQTP_CODE, 
-           T.RQTP_DESC, 
-           T.RQTT_CODE,
-           T.RQTT_DESC,
-           T.RQID, 
-           T.RQST_RQID,
-           T.RQST_DATE,
-           T.SAVE_DATE,
-           T.CRET_BY,
-           T.RQST_DESC,
---           T.RQST_RQID,
---           T.CASH_CODE,
-           /*T.NAME_DNRM,
-           T.FILE_NO,
-           T.CELL_PHON_DNRM,*/
-           --T.FIGH_FILE_NO,
-           T.TOTL_AMNT,
-           ISNULL((SELECT SUM(pm.AMNT) FROM Payment_Method Pm WHERE pm.PYMT_CASH_CODE = T.CASH_CODE AND pm.PYMT_RQST_RQID = T.PYMT_RQST_RQID) , 0) AS TOTL_RCPT_AMNT,
-           ISNULL((SELECT SUM(pc.AMNT) FROM Payment_Discount pc WHERE pc.PYMT_CASH_CODE = T.CASH_CODE AND pc.PYMT_RQST_RQID = T.PYMT_RQST_RQID), 0) AS TOTL_DSCT_AMNT
-     FROM (
-   SELECT  r.RQTP_CODE, 
-           rqtp.RQTP_DESC, 
-           r.RQTT_CODE,
-           rqtt.RQTT_DESC,
-           r.RQID, 
-           r.RQST_RQID,
-           r.RQST_DATE,
-           r.SAVE_DATE,
-           r.RQST_DESC,
-           r.CRET_BY, 
-           P.RQST_RQID AS PYMT_RQST_RQID,           
-           P.CASH_CODE,
-           /*F.NAME_DNRM,
-           F.File_No,
-           F.Cell_Phon_Dnrm,*/
-           --pd.FIGH_FILE_NO,
-           ISNULL(SUM((pd.EXPN_PRIC + ISNULL(pd.EXPN_EXTR_PRCT, 0)) * pd.QNTY), 0) AS TOTL_AMNT
-     FROM  dbo.Request r
-     INNER JOIN dbo.Request_Row rr ON r.RQID = rr.RQST_RQID 
-     INNER JOIN dbo.Request_Type rqtp ON r.RQTP_CODE = rqtp.CODE 
-     INNER JOIN dbo.Requester_Type rqtt ON r.RQTT_CODE = rqtt.CODE
-     INNER JOIN dbo.Fighter f ON rr.FIGH_FILE_NO = f.FILE_NO
-     LEFT OUTER JOIN dbo.Payment p ON p.RQST_RQID = r.RQID
-     LEFT OUTER JOIN dbo.Payment_Detail pd ON pd.PYMT_CASH_CODE = p.CASH_CODE AND pd.PYMT_RQST_RQID = p.RQST_RQID
-     --LEFT OUTER JOIN dbo.Payment_Method pm ON pm.PYMT_CASH_CODE = p.CASH_CODE AND pm.PYMT_RQST_RQID = p.RQST_RQID
-     --LEFT OUTER JOIN dbo.Payment_Discount pc ON pc.PYMT_CASH_CODE = p.CASH_CODE AND pc.PYMT_RQST_RQID = p.RQST_RQID
-   WHERE  (rr.RECD_STAT = '002') 
-     AND  (r.RQST_STAT = '002')
-     AND  (f.CONF_STAT = '002')
-     AND  (@FileNo IS NULL OR f.FILE_NO = @FileNo)
-     GROUP BY r.RQTP_CODE, 
-           rqtp.RQTP_DESC, 
-           r.RQTT_CODE,
-           rqtt.RQTT_DESC,
-           r.RQID, 
-           r.RQST_RQID,
-           r.RQST_DATE,
-           r.SAVE_DATE,
-           r.CRET_BY,
-           r.RQST_DESC,
-           P.RQST_RQID,
-           P.CASH_CODE
-           /*F.NAME_DNRM,
-           F.FILE_NO,
-           F.CELL_PHON_DNRM*/
-           --Pd.FIGH_FILE_NO
-   ) T
-)
+    AS RETURN
+    ( SELECT    T.RQTP_CODE ,
+                T.RQTP_DESC ,
+                T.RQTT_CODE ,
+                T.RQTT_DESC ,
+                T.RQID ,
+                T.RQST_RQID ,
+                T.RQST_DATE ,
+                T.SAVE_DATE ,
+                T.CRET_BY ,
+                T.RQST_DESC ,
+                T.TOTL_AMNT ,
+                ISNULL(( SELECT SUM(Pm.AMNT)
+                         FROM   Payment_Method Pm
+                         WHERE  Pm.PYMT_CASH_CODE = T.CASH_CODE
+                                AND Pm.PYMT_RQST_RQID = T.PYMT_RQST_RQID
+                       ), 0) AS TOTL_RCPT_AMNT ,
+                ISNULL(( SELECT SUM(pc.AMNT)
+                         FROM   Payment_Discount pc
+                         WHERE  pc.PYMT_CASH_CODE = T.CASH_CODE
+                                AND pc.PYMT_RQST_RQID = T.PYMT_RQST_RQID
+                       ), 0) AS TOTL_DSCT_AMNT
+      FROM      ( SELECT    r.RQTP_CODE ,
+                            rqtp.RQTP_DESC ,
+                            r.RQTT_CODE ,
+                            rqtt.RQTT_DESC ,
+                            r.RQID ,
+                            r.RQST_RQID ,
+                            r.RQST_DATE ,
+                            r.SAVE_DATE ,
+                            r.RQST_DESC ,
+                            r.CRET_BY ,
+                            p.RQST_RQID AS PYMT_RQST_RQID ,
+                            p.CASH_CODE ,
+                            ISNULL(SUM(( pd.EXPN_PRIC
+                                         + ISNULL(pd.EXPN_EXTR_PRCT, 0) )
+                                       * pd.QNTY), 0) AS TOTL_AMNT
+                  FROM      dbo.Request r
+                            INNER JOIN dbo.Request_Row rr ON r.RQID = rr.RQST_RQID
+                            INNER JOIN dbo.Request_Type rqtp ON r.RQTP_CODE = rqtp.CODE
+                            INNER JOIN dbo.Requester_Type rqtt ON r.RQTT_CODE = rqtt.CODE
+                            INNER JOIN dbo.Fighter f ON rr.FIGH_FILE_NO = f.FILE_NO
+                            LEFT OUTER JOIN dbo.Payment p ON p.RQST_RQID = r.RQID
+                            LEFT OUTER JOIN dbo.Payment_Detail pd ON pd.PYMT_CASH_CODE = p.CASH_CODE
+                                                              AND pd.PYMT_RQST_RQID = p.RQST_RQID
+                  WHERE     ( rr.RECD_STAT = '002' )
+                            AND ( r.RQST_STAT = '002' )
+                            AND ( f.CONF_STAT = '002' )
+                            AND ( r.RQTP_CODE NOT IN ( '020' ) )
+                            AND ( @FileNo IS NULL
+                                  OR f.FILE_NO = @FileNo
+                                )
+                  GROUP BY  r.RQTP_CODE ,
+                            rqtp.RQTP_DESC ,
+                            r.RQTT_CODE ,
+                            rqtt.RQTT_DESC ,
+                            r.RQID ,
+                            r.RQST_RQID ,
+                            r.RQST_DATE ,
+                            r.SAVE_DATE ,
+                            r.CRET_BY ,
+                            r.RQST_DESC ,
+                            p.RQST_RQID ,
+                            p.CASH_CODE
+                ) T
+      UNION ALL
+      SELECT    r.RQTP_CODE ,
+                rt.RQTP_DESC ,
+                r.RQTT_CODE ,
+                rqt.RQTT_DESC ,
+                r.RQID ,
+                r.RQST_RQID ,
+                r.RQST_DATE ,
+                r.SAVE_DATE ,
+                r.CRET_BY ,
+                r.RQST_DESC ,                
+                CASE g.DPST_STAT
+                  WHEN '001' THEN -1 * g.AMNT
+                  ELSE g.AMNT
+                END AS TOTL_AMNT ,
+                CASE g.DPST_STAT
+                  WHEN '001' THEN -1 * g.AMNT
+                  ELSE g.AMNT
+                END AS TOTL_RCPT_AMNT ,
+                0 AS TOTL_DSCT_AMNT
+      FROM      dbo.Request_Type rt ,
+                dbo.Requester_Type rqt ,
+                dbo.Request r ,
+                dbo.Request_Row rr ,
+                dbo.Gain_Loss_Rial g
+      WHERE     r.RQTP_CODE = '020'
+                AND r.RQST_STAT = '002'
+                AND rt.CODE = r.RQTP_CODE
+                AND rqt.CODE = r.RQTT_CODE
+                AND r.RQID = rr.RQST_RQID
+                AND rr.RQST_RQID = g.RQRO_RQST_RQID
+                AND rr.RWNO = g.RQRO_RWNO
+                AND rr.FIGH_FILE_NO = @FileNo
+    );
 GO
